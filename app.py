31b0ea9be10e3ee37d85f6e7e3e8969ffe2eb081
Tanisha Kshirsagar <kshirsagartanisha15@gmail.com>
2026-05-27 13:47:12 +0530

    fix: bind to 0.0.0.0 for render

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def on_join(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
